397a4313d327735a9becd1182c4e4472fba679bb
arup saha <arup.saha@indusnet.co.in>
2022-05-04 16:21:51 +0530

    basic api in node js CRUD operation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dbConnect = require('./mongodbConnect');
+const mongodb = require('mongodb');
 const app = express();
 
 app.use(express.json());
@@ -16,6 +17,23 @@ app.post('/', async(req, res)=>{
     let data = await dbConnect();
     let result = await data.insertOne(req.body)
     res.send(result)
+})
+
+
+app.put('/:name', async(req, res)=>{
+    let data = await dbConnect();
+    let result = await data.updateOne(
+        { name: req.params.name },
+        { $set: req.body }
+    )
+    res.send(result );  
+})
+
+app.delete('/:id', async(req, res)=>{
+    // console.log(res.params.id);
+    const data = await dbConnect();
+    const result = await data.deleteOne({_id:new mongodb.ObjectId(req.params.id)})
+    res.send(result);
 })
 
 app.listen(5000);
